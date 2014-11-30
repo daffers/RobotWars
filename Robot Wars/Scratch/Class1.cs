f4f79ddee10eb59518fa35b5a89e8db1cf6e7a44
Dafydd Giddins <dafydd.giddins@live.co.uk>
2014-11-30 15:57:07 +0000

    Created initial batch of RobotWarrior tests, identified need to create the concept of a command. Command parsing tests to follow.

--- Robot Wars/Scratch/Class1.cs
+++ Robot Wars/Scratch/Class1.cs
@@ -1,4 +1,7 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
+using System.Linq.Expressions;
+using System.Runtime.InteropServices;
 using NUnit.Framework;
 using RobotWars;
 
@@ -31,6 +34,68 @@ namespace Scratch
             Assert.That(finalPositions.Count, Is.EqualTo(2));
             Assert.That(finalPositions[0], Is.EqualTo(rover1ExpectedFinalPositon));
             Assert.That(finalPositions[0], Is.EqualTo(rover2ExpectedFinalPositon));
+        }
+    }
+
+    [TestFixture]
+    public class WarriorRobotTests
+    {
+        [Test]
+        public void CanCreateAWarriorRobot()
+        {
+            var robot = new WarriorRobot();
+        }
+
+        [Test]
+        public void CanUploadArenaToRobot()
+        {
+            var robot = new WarriorRobot();
+            robot.UploadArena(new Arena(1, 1));
+        }
+
+        [Test]
+        public void CanStartARobotOnAnArenaPosition()
+        {
+            var robot = new WarriorRobot();
+            robot.UploadArena(new Arena(1,1));
+            robot.StartAt(0, 0, "N");
+        }
+
+        [Test]
+        public void CannotStartARobotWhenNoArenaUploaded()
+        {
+            var robot = new WarriorRobot();
+            Assert.Throws<WarriorRobot.ArenaNotUploadedException>(() => robot.StartAt(0, 0, "N"));
+        }
+
+        [Test]
+        public void CanProvideCommandsToRobot()
+        {
+
+        }
+    }
+
+    public class WarriorRobot
+    {
+        private Arena _arena;
+
+        public void StartOn(Arena arena)
+        {
+        }
+
+        public void UploadArena(Arena arena)
+        {
+            _arena = arena;
+        }
+
+        public void StartAt(int xCord, int yCord, string direction)
+        {
+            if (_arena == null)
+                throw new ArenaNotUploadedException();
+        }
+
+        public class ArenaNotUploadedException: Exception
+        {
         }
     }
 }
