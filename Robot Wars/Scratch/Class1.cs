f1649eed8b5de85ef70576302e3f3d392c5f7db4
Dafydd Giddins <dafydd.giddins@live.co.uk>
2014-11-30 17:00:14 +0000

    Command Parsing tests complete for movement commands

--- Robot Wars/Scratch/Class1.cs
+++ Robot Wars/Scratch/Class1.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Linq;
 using System.Linq.Expressions;
 using System.Runtime.InteropServices;
 using NUnit.Framework;
@@ -96,6 +97,112 @@ namespace Scratch
 
         public class ArenaNotUploadedException: Exception
         {
+        }
+    }
+
+    [TestFixture]
+    public class CommandParsingTests
+    {
+        [Test]
+        public void CanCreateACommanParser()
+        {
+            var commandParser = new CommandParser();
+        }
+
+        [Test]
+        public void CanParseRobotMovementCommandsIntoAListOfCommands()
+        {
+            const string commandString = "LRM";
+            var commandParser = new CommandParser();
+            IEnumerable<RobotCommand> commands = commandParser.ParseRoverMovements(commandString);
+
+            Assert.That(commands.Count(), Is.EqualTo(commandString.Length));
+        }
+
+        [Test]
+        public void LIsParsedToATurnLeftCommand()
+        {
+            const string commandString = "L";
+            var commandParser = new CommandParser();
+            IEnumerable<RobotCommand> commands = commandParser.ParseRoverMovements(commandString);
+
+            Assert.That(commands.First(), Is.TypeOf<TurnLeft>());
+        }
+
+        [Test]
+        public void RIsParsedToATurnRightCommand()
+        {
+            const string commandString = "R";
+            var commandParser = new CommandParser();
+            IEnumerable<RobotCommand> commands = commandParser.ParseRoverMovements(commandString);
+
+            Assert.That(commands.First(), Is.TypeOf<TurnRight>());
+        }
+
+        [Test]
+        public void MIsParsedToAMoveForwardCommand()
+        {
+            const string commandString = "M";
+            var commandParser = new CommandParser();
+            IEnumerable<RobotCommand> commands = commandParser.ParseRoverMovements(commandString);
+
+            Assert.That(commands.First(), Is.TypeOf<MoveForwards>());
+        }
+
+        [Test]
+        public void MultiCommandParseTest()
+        {
+            const string commandString = "MRRMLLRM";
+            var commandParser = new CommandParser();
+            IList<RobotCommand> commands = commandParser.ParseRoverMovements(commandString).ToList();
+
+            Assert.That(commands.Count(), Is.EqualTo(commandString.Length));
+            Assert.That(commands[0], Is.TypeOf<MoveForwards>());
+            Assert.That(commands[1], Is.TypeOf<TurnRight>());
+            Assert.That(commands[2], Is.TypeOf<TurnRight>());
+            Assert.That(commands[3], Is.TypeOf<MoveForwards>());
+            Assert.That(commands[4], Is.TypeOf<TurnLeft>());
+            Assert.That(commands[5], Is.TypeOf<TurnLeft>());
+            Assert.That(commands[6], Is.TypeOf<TurnRight>());
+            Assert.That(commands[7], Is.TypeOf<MoveForwards>());
+        }
+    }
+
+    public class MoveForwards : RobotCommand
+    {
+    }
+
+    public class TurnRight : RobotCommand
+    {
+    }
+
+    public class TurnLeft : RobotCommand
+    {
+    }
+
+    public class RobotCommand
+    {
+    }
+
+    public class CommandParser
+    {
+        public IEnumerable<RobotCommand> ParseRoverMovements(string commandString)
+        {
+            foreach (char command in commandString)
+            {
+                yield return ParseCommand(command);
+            }
+        }
+
+        private RobotCommand ParseCommand(char command)
+        {
+            if (command == 'L')
+                return new TurnLeft();
+            if (command == 'R')
+                return new TurnRight();
+            if (command == 'M')
+                return new MoveForwards();
+            throw new ArgumentException();
         }
     }
 }
