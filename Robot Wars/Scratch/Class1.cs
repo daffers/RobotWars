bd68f5000ab5061ab33c97453004c866db525223
Dafydd Giddins <dafydd.giddins@live.co.uk>
2014-11-30 09:36:37 +0000

    Acceptance test written and defined the interaction interface.

---
+++ Robot Wars/Scratch/Class1.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections.Generic;
+using NUnit.Framework;
+
+namespace Scratch
+{
+    [TestFixture]
+    public class AcceptanceTests
+    {
+        [Test]
+        [Ignore("Awaiting implementation")]
+        public void AcceptanceCriteriaPassesThroughConsole()
+        {
+            const string gridSize = "5 5";
+            const string rover1StartingVector = "1 2 N";
+            const string rover1MovementSequence = "LMLMLMLMM";
+            const string rover2StartingVector = "3 3 E";
+            const string rover2MovementSequence = "MMRMMRMRRM";
+
+            var controlConsole = new ControlConsole();
+
+            controlConsole.SetGrid(gridSize);
+            controlConsole.SetRoverPositionAndCommands(rover1StartingVector, rover1MovementSequence);
+            controlConsole.SetRoverPositionAndCommands(rover2StartingVector, rover2MovementSequence);
+
+            List<string> finalPositions = controlConsole.GetFinalPositions();
+
+            const string rover1ExpectedFinalPositon = "1 3 N";
+            const string rover2ExpectedFinalPositon = "5 1 E";
+
+            Assert.That(finalPositions.Count, Is.EqualTo(2));
+            Assert.That(finalPositions[0], Is.EqualTo(rover1ExpectedFinalPositon));
+            Assert.That(finalPositions[0], Is.EqualTo(rover2ExpectedFinalPositon));
+        }
+    }
+
+    public class ControlConsole
+    {
+        public void SetGrid(string gridSize)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public void SetRoverPositionAndCommands(string rover1StartingVector, string rover1MovementSequence)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public List<string> GetFinalPositions()
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+}
